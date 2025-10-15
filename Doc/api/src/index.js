a366e94039967388471f0164e56dab53c31aebda
Pau8888888 <paalca1@jviladoms.cat>
2025-10-15 09:23:30 +0200

    Afegits nous fitxers de productes i actualitzades rutes

--- Doc/api/src/index.js
+++ Doc/api/src/index.js
@@ -1,9 +1,9 @@
 require('dotenv').config();
 const express = require('express');
 const connectDB = require('./config/db');
-const app = express();
+const productRoutes = require('./routes/productRoutes'); // Importem les rutes const app = express(); 
 app.use(express.json());
 connectDB();
 app.get('/', (req, res) => res.send('API Ecommerce en marxa'));
-const PORT = process.env.PORT || 4000;
-app.listen(PORT, () => console.log(`Servidor escoltant al port ${PORT}`)); 
+// Muntem les rutes de productes sota el prefix '/api/products' 
+app.use('/api/products', productRoutes); 
